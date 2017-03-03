c6473814dff588ba3cce828ea8f9752b5c2ea5e4
github username <github email address>
2017-03-03 10:36:55 -0500

    homepage and session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    auth = requestenv["omniauth.auth"]
+    if auth
+      @user = User.sign_in_with_auth(auth)
+      session[:id] = @user.id
+    else
+      @user = User.find_by(email: params[:email])
+      if @user == @user.authenticate(params[:password]) && @user.provider.nil?
+        session[:id] = @user.id
+      end
+    end
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    session.delete(:id)
+    redirect_to '/'
+  end
+
+end
