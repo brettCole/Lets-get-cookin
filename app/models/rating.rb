0588e948be3696a18e4548b8748d18f1b47232f2
github username <github email address>
2017-04-09 22:39:31 -0400

    highest rated recipe method

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,9 +3,10 @@ class Rating < ApplicationRecord
   belongs_to :user
   validates :rating, :review, presence: true
 
-  scope :most_popular, -> {
-    joins(:recipe).group(:recipe_id).sum(:rating)
-    binding.pry
-  }
+  # def self.highest_rated
+  #   found_recipe = Recipe.find(self.joins(:recipe).group(:recipe_id).sum(:rating).max_by { |key, value| value }[0])
+  #   found_recipe.title
+  # end
+  scope :highest_rated, -> { Recipe.find(self.joins(:recipe).group(:recipe_id).sum(:rating).max_by { |key, value| value }[0]) }
 
 end
